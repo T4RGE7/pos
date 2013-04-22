759adb5032996cbc7a577641069c12fd3cd18410
Stephen <sdc22645@email.vccs.du>
2013-04-22 11:27:39 -0400

    Began work on implementing logo

--- FinalProjectPointOfSale/src/pointOfSale/LogInGUI.java
+++ FinalProjectPointOfSale/src/pointOfSale/LogInGUI.java
@@ -1,9 +1,6 @@
 package pointOfSale;
-import javax.swing.BorderFactory;
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,18 +8,28 @@ public class LogInGUI extends JPanel implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
 	private static final Color DARK_CHAMPAGNE = new Color(194, 178, 128);
+	private static final Color DARK_GREEN = new Color(0,100,0);
 
 	private JPanel halfPanel = new JPanel(new GridLayout(1,2));
 	private JPanel quarterPanel = new JPanel(new GridLayout(2,1));
 	private JPanel exitPanel = new JPanel(new GridLayout(4,2));
+	private JPanel logoPanel = new JPanel(new GridLayout(1,1));
 	private KeyPad numberPad = new KeyPad();
 	private MenuButton exitButton = new MenuButton("Exit","Exit",this);
+	private ImageIcon logo = new ImageIcon("Files/Icons/logoBig.jpg");
+	private JLabel logoLabel = new JLabel(logo,SwingConstants.CENTER);
 	
 	LogInGUI()
 	{	
 		setLayout(new GridLayout(1,2));
 		setBackground(DARK_CHAMPAGNE);
 		setBorder(BorderFactory.createLoweredBevelBorder());
+		
+		logoLabel.setVerticalAlignment(SwingConstants.CENTER);
+		logoPanel.setBackground(DARK_CHAMPAGNE);
+		logoPanel.setBorder(BorderFactory.createEtchedBorder());
+		//logoPanel.setBorder(BorderFactory.createMatteBorder(50,50,50,50, DARK_CHAMPAGNE));
+		logoPanel.add(logoLabel); 
 		
 		Tools.addBlankSpace(exitPanel,7);
 		exitPanel.add(exitButton);
@@ -37,7 +44,7 @@ public class LogInGUI extends JPanel implements ActionListener
 		halfPanel.add(quarterPanel);
 		halfPanel.setBackground(DARK_CHAMPAGNE);
 		
-		Tools.addBlankSpace(this,1);
+		add(logoPanel);
 		add(halfPanel);
 	}
 	
